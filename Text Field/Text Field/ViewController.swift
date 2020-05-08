41a41daf4478d980ad71eb55e1d27e0ca85dacf2
Al Mustakim <badhonmdabdullah@gmail.com>
2020-05-08 23:32:25 +0600

    first commit

---
+++ Text Field/Text Field/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Text Field
+//
+//  Created by AL Mustakim on 2/5/20.
+//  Copyright © 2020 AL Mustakim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        dismissKey()
+    }
+
+
+}
+extension UIViewController {
+    func dismissKey()
+    {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer( target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard()
+    {
+        view.endEditing(true)
+    }
+}
+
+
